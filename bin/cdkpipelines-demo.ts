cc952d15ff07d9e63fd191dd1525931d1809523d
Vsevolod Geraskin <sev@Vsevolods-MacBook-Pro.local>
2021-12-01 17:24:24 -0800

    Update Github repo name and AWS account id

--- bin/cdkpipelines-demo.ts
+++ bin/cdkpipelines-demo.ts
@@ -5,7 +5,7 @@ import { CdkpipelinesDemoPipelineStack } from "../lib/cdkpipelines-demo-pipeline
 const app = new App();
 
 new CdkpipelinesDemoPipelineStack(app, "CdkpipelinesDemoPipelineStack", {
-  env: { account: "ACCOUNT", region: "us-west-2" },
+  env: { account: "047670219462", region: "us-west-2" },
 });
 
 app.synth();
